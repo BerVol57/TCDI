c208934328328a8d1e119d64db94d7a79253b8f8
Беркута Володимир Юрійович <berkutavolodymyr125@gmail.com>
2024-04-16 03:16:49 +0300

    Add code image and send this image to user

--- code_decode_img.py
+++ code_decode_img.py
@@ -1,14 +1,67 @@
 import numpy as np
 import cv2 as cv
+import math
 
 
 class Image:
-    def __init__(self, bytes_img: bytes):
+    def __init__(self):
+        self.img = None
+        self.watermark = None
+        self.code = False
+        self.decode = False
+        self.is_watermark = False
+
+    def set_value(self, bytes_img: bytes):
         pre_img = np.frombuffer(bytes_img, dtype=np.uint8)
         self.img = cv.imdecode(pre_img, cv.IMREAD_UNCHANGED)
 
-    def reset_img(self, bytes_img: bytes):
-        pre_img = np.frombuffer(bytes_img, dtype=np.uint8)
-        self.img = cv.imdecode(pre_img, cv.IMREAD_UNCHANGED)
+    def show(self):
+        if self.img:
+            cv.imshow("img", self.img)
+            cv.waitKey(0)
+        else:
+            print("Image value is none")
 
+    def set_watermark(self, bytes_watermark: bytes):
+        pre_watermark = np.frombuffer(bytes_watermark, dtype=np.uint8)
+        self.watermark = cv.imdecode(pre_watermark, cv.IMREAD_UNCHANGED)
 
+        pre_mask = np.empty_like(self.img)
+        water_wight = self.watermark.shape[0]
+        water_height = self.watermark.shape[1]
+
+        for i in range(math.ceil(self.img.shape[0] / water_wight)):
+
+            if (i + 1) * water_wight > self.img.shape[0]:
+                border_width = self.img.shape[0]
+            else:
+                border_width = (i + 1) * water_wight
+
+            for j in range(math.ceil(self.img.shape[1] / water_height)):
+
+                if (j + 1) * water_height > self.img.shape[1]:
+                    border_height = self.img.shape[1]
+                else:
+                    border_height = (j + 1) * water_height
+
+                pre_mask[i * water_wight: border_width, j * water_height:border_height] = \
+                    self.watermark[0: border_width - i * water_wight, 0: border_height - j * water_height]
+
+        gray_mask = cv.cvtColor(pre_mask, cv.COLOR_BGR2GRAY)
+        (_, bw_mask) = cv.threshold(gray_mask,
+                                    127,
+                                    255,
+                                    cv.THRESH_BINARY)
+        mask = bw_mask // 255
+
+        red_chanel = self.img[:, :, 1] % 2
+        self.img[:, :, 1] += -red_chanel + mask.astype('uint8')
+        _, img_encode = cv.imencode(".jpg", self.img)
+        return img_encode.tobytes()
+
+    def get_watermark(self, bytes_img: bytes):
+        self.set_value(bytes_img)
+        red_chanel = self.img[:, :, 1]%2
+        get_watermark_img = (red_chanel*255).astype('uint8')
+        _, get_watermark_img_encode = cv.imencode(".jpg", get_watermark_img)
+        return get_watermark_img_encode.tobytes()
--- main.py
+++ main.py
@@ -1,58 +1,103 @@
-import cv2
-import telebot
 import requests
-import cv2 as cv
-import numpy as np
+import telebot
+
+from code_decode_img import *
 
 with open("API_TOKEN.txt", "r") as f:
     API_TOKEN = f.read()
-bot = telebot.TeleBot(API_TOKEN)
+BOT = telebot.TeleBot(API_TOKEN)
 
 markup = telebot.types.ReplyKeyboardMarkup(selective=False)
-item_code = telebot.types.KeyboardButton("Закодувати")
-item_decode = telebot.types.KeyboardButton("Декодувати")
+item_code = telebot.types.KeyboardButton("/Закодувати")
+item_decode = telebot.types.KeyboardButton("/Декодувати")
 markup.row(item_code, item_decode)
 
+img = Image()
 
-@bot.message_handler(commands=['start'])
+
+def reset():
+    img.img = None
+    img.code, img.decode, img.is_watermark = False, False, False
+
+
+@BOT.message_handler(commands=['start'])
 def send_welcome(message):
+    reset()
     markup.selective = True
-    bot.send_message(chat_id=message.chat.id,
+    BOT.send_message(chat_id=message.chat.id,
                      text="""
                      Привіт, цей бот написаний для закодування чорно-білого зображення як Watermark для іншого.
                      \nТакож декодування зображення для отримання подібного Watermark.
                      \nВиберіть функцію:
                      """,
                      reply_markup=markup)
+    # photo_test = open("test/slime.png", "rb")
+    # BOT.send_photo(chat_id=message.chat.id, photo=photo_test)
+    # photo_test.close()
 
 
-@bot.message_handler(commands=['Декодувати'])
+@BOT.message_handler(commands=['Закодувати'])
 def send_welcome(message):
-    bot.send_message(chat_id=message.chat.id,
-                     text=""" Надішліть фотографію, що буде закодовано """,
+    BOT.send_message(chat_id=message.chat.id,
+                     text=""" Надішліть фотографію, що буде закодовано: """,
                      reply_markup=telebot.types.ReplyKeyboardRemove())
+    img.code = True
 
 
-@bot.message_handler(content_types=['photo'])
+@BOT.message_handler(commands=['Декодувати'])
+def send_welcome(message):
+    BOT.send_message(chat_id=message.chat.id,
+                     text=""" Надішліть фотографію для декодування: """,
+                     reply_markup=telebot.types.ReplyKeyboardRemove())
+    img.decode = True
+
+
+@BOT.message_handler(content_types=['photo'])
 def handle_image(message):
+    print(img.code, img.decode, img.is_watermark)
     # Get the file ID of the photo
     file_id = message.photo[-1].file_id
 
     # Get the file path using the file ID
-    img_info = bot.get_file(file_id)
+    img_info = BOT.get_file(file_id)
     img_path = img_info.file_path
 
     # Download the image
     image_url = f"https://api.telegram.org/file/bot{API_TOKEN}/{img_path}"
     response = requests.get(image_url)
-    pre_img = np.frombuffer(response.content, dtype=np.uint8)
-    img = cv2.imdecode(pre_img, cv.IMREAD_UNCHANGED)
-    cv.imshow("test", img)
-    k = cv.waitKey(0)
 
-    # Reply to the user
-    bot.reply_to(message, "Зображення збережено.")
+    if img.code:
+        print("Code")
+        try:
+            img.set_value(response.content)
+            BOT.reply_to(message, "Надішліть Watermark(чорно-біле зображення).")
+            img.code = False
+            img.is_watermark = True
+        except Exception as e:
+            BOT.send_message(chat_id=message.chat.id,
+                             text=f"Помилка: {e}")
+
+    elif img.decode:
+        print("Decode")
+        try:
+            img.decode = False
+            BOT.send_photo(chat_id=message.chat.id,
+                           photo=img.get_watermark(response.content))
+        except Exception as e:
+            BOT.send_message(chat_id=message.chat.id,
+                             text=f"Помилка: {e}")
+
+    elif img.is_watermark:
+        print("Watermark")
+        try:
+            # text = img.set_watermark(response.content)
+            BOT.send_photo(chat_id=message.chat.id,
+                           photo=img.set_watermark(response.content))
+            img.is_watermark = False
+        except Exception as e:
+            BOT.send_message(chat_id=message.chat.id,
+                             text=f"Помилка: {e}")
 
 
 if __name__ == "__main__":
-    bot.polling()
+    BOT.polling()
